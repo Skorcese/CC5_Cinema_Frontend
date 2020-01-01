77eec8fc5f0f4758d0920f3cdd8ff0933410e194
Kamil Żogło <kamilzoglo@gmail.com>
2020-01-01 21:03:51 +0100

    Changed path to the App component.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-import App from './components/App';
-import reducers from './reducers'
+import App from './components/App/App';
+import reducers from './reducers';
 
-const store = createStore(reducers, applyMiddleware(thunk))
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.querySelector('#root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
 );
