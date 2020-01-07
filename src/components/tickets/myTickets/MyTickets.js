1a34c5cdb6c928c1e6ce9b0cef27dc9a1419f10a
Kamil <zoglokamil@gmail.com>
2020-01-07 18:29:48 +0100

    Files for rendering user's tickets.

---
+++ src/components/tickets/myTickets/MyTickets.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import end from '../../../apis/index';
+import RenderMyTickets from './RenderMyTickets';
+import './MyTickets.css';
+
+class MyTickets extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //Temporarily hardcoded user id
+      userId: '5e0a588e1f912e227c2099cf',
+      reservations: []
+    };
+  }
+
+  async componentDidMount() {
+    const reservations = await end.get(
+      `/api/reservation/${this.state.userId}/default`
+    );
+    this.setState({
+      ...this.state,
+      reservations: reservations.data
+    });
+    console.log(this.state.reservations);
+  }
+
+  render() {
+    return (
+      <div className="page-wrapper">
+        <RenderMyTickets data={this.state.reservations} />
+      </div>
+    );
+  }
+}
+
+export default MyTickets;
---
+++ src/components/tickets/myTickets/RenderMyTickets.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './MyTickets.css';
+import ScreeningInfo from '../util_components/ScreeningInfo';
+
+function fullDescrAndTooltipText(refDescr, fullDescr, descr, refTooltip) {
+  if (refDescr.current.innerText.length === 33) {
+    refDescr.current.innerText = fullDescr;
+    refTooltip.current.innerText = 'Zwiń opis';
+  } else {
+    refDescr.current.innerText = descr;
+    refTooltip.current.innerText = 'Rozwiń opis';
+  }
+}
+
+const RenderMyTickets = props => {
+  console.log(props.data);
+  let toRender;
+
+  if (props.data.length > 0) {
+    toRender = props.data.map(reservation => {
+      let screening = {};
+      let seat;
+      let descr;
+      let fullDescr;
+      screening = reservation.screening_id;
+      seat = reservation.seat_id;
+      fullDescr = reservation.screening_id.movie_id.description;
+
+      if (screening.movie_id.description.length > 30) {
+        descr = `${screening.movie_id.description.slice(0, 30)}...`;
+      } else descr = screening.movie_id.description;
+
+      let refDescr = React.createRef();
+      let refTooltip = React.createRef();
+
+      return (
+        <div
+          className="tickets-wrapper"
+          key={reservation._id || reservation.data._id}
+        >
+          <img
+            className="moviePoster"
+            src={`../../../img/home/${screening.movie_id.imageUrl}`}
+            alt={screening.movie_id.title + ' poster'}
+          />
+          <div className="utils">
+            <h3 className="movieTitle">{screening.movie_id.title}</h3>
+            <p
+              className="description tooltip"
+              onClick={() =>
+                fullDescrAndTooltipText(refDescr, fullDescr, descr, refTooltip)
+              }
+            >
+              <span>{'Opis: '}</span>
+              <span className="descrSpan" ref={refDescr}>
+                {descr}
+              </span>
+              <span className="tooltipText" ref={refTooltip}>
+                {'Rozwiń opis'}
+              </span>
+            </p>
+            <ScreeningInfo seat={seat} screening={screening} />
+          </div>
+        </div>
+      );
+    });
+  } else {
+    toRender = <div className="zeroTickets">Brak biletów do wyświetlenia</div>;
+  }
+
+  return toRender;
+};
+
+export default RenderMyTickets;
