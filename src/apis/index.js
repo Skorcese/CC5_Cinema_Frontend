7fc20d44d255211d44cae123f0e1227b29c39716
Klaudia <gora.klaudia@gmail.com>
2020-01-09 18:22:53 +0100

    init 4

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const end = axios.create({
-  baseURL: 'wss://mini-kino.herokuapp.com',
+  baseURL: 'https://mini-kino.herokuapp.com',
   timeout: 3000,
   headers: {
     'Access-Control-Allow-Origin': '*',
